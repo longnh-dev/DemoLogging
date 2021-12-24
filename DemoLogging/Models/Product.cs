3c2bbc1a6d7546d90a7818efdf7d0662cf5231fe
the canxi <hoanglongcnttk42b2@gmail.com>
2021-12-24 17:51:18 +0700

    Add project files.

---
+++ DemoLogging/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoLogging.Models
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ DemoLogging/Models/ProductContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoLogging.Models
+{
+    public class ProductContext : DbContext
+    {
+        // public static readonly ILoggerFactory loggerFactory = new LoggerFactory().AddConsole((_, ___) => true);
+
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
